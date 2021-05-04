6c84bc23df87f6059c22468231bf9a17ef05f4dc
Heshie Brody <4hbrody@gmail.com>
2021-05-04 01:35:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,23 +1,28 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index {|elm, i| puts "#{i+1}. #{elm}"}
+  array.each_with_index do |i, index|
+    puts "#{index += 1} #{i}"
+  end
 end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.collect{|word| word.capitalize << "!"}
+  array.map! do |i|
+    i.capitalize
+  end
+  new_array = []
+  array.each do |i|
+    new_array << i += "!"
+  end
+  return new_array
 end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any? { |call| call.length > 4 }
+  array.any? { |i| i.length > 4}
 end
 
 def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
 
-  ingredients.find do |maybe_cheese|
-    cheese_types.include?(maybe_cheese)
-  end
 end
